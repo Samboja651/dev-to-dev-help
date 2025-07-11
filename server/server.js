137e9fa0c8f379cb9a0314b3b52d1afe2987bfab
Granton <waribekihiko651@gmail.com>
2025-07-11 16:48:29 +0300

    server setup

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const ticketRoutes = require ('./routes/tickets');
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+
+// routes
+app.use('/api/tickets', ticketRoutes);
+
+// db connection
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('MongoDB connected');
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("DB connection failed: ", err.message);
+    });
